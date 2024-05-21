1889693bbe59a0a1482c5c2b7e15ae63e98d778a
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-05-21 22:00:33 +0300

    add medicine service

---
+++ ApplicationCore/Services/MedicineService/MedicineService.cs
@@ -0,0 +1,27 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services.MedicineService
+{
+    public class MedicineService
+    {
+        private readonly IUnitOfWork<Medicine> _medicineUnitOfWork;
+
+        public MedicineService(IUnitOfWork<Medicine> medicineUnitOfWork)
+        {
+            _medicineUnitOfWork = medicineUnitOfWork;
+        }
+
+        public Medicine GetMedicineDetails(int id)
+        {
+
+            return _medicineUnitOfWork.Entity.GetById(id , i => i.MedicineIngredients , i => i.Ingredients );
+            
+        }
+    }
+}
