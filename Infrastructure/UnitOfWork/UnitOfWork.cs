3de9679164048d5a58c84dfad66a6331a05b841c
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-05-21 22:03:51 +0300

    implement unit of work

--- Infrastructure/UnitOfWork/UnitOfWork.cs
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,33 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.UnitOfWork
 {
-    class UnitOfWork
+    public class UnitOfWork<T> :IUnitOfWork<T> where T : EntityBase
     {
+        private readonly DbContext _context;
+        private IGenericRepository<T> _entity;
+        public UnitOfWork(DbContext context)
+        {
+            _context = context;
+            _entity = new GenericRepository<T>(context);
+        }
+
+        public IGenericRepository<T> Entity
+        {
+            get
+            {
+
+                return _entity ?? (_entity = new GenericRepository<T>(_context));
+            }
+
+
+
+
+        }
+
+        void IUnitOfWork<T>.Save()
+        {
+            _context.SaveChanges();
+        }
+
     }
 }
