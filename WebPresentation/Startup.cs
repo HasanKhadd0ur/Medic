83a204b8ec71b535a0b44d32f28f363a274a575f
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-05-21 22:08:21 +0300

    u

--- WebPresentation/Startup.cs
+++ WebPresentation/Startup.cs
@@ -1,6 +1,13 @@
+using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Repository;
+using Infrastructure.UnitOfWork;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +30,30 @@ namespace WebPresentation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+
+            
+            services.AddScoped(typeof(DbContext), typeof(MedicDbContext));
+            services.AddScoped(typeof(IUnitOfWork<>),typeof(UnitOfWork<>));
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+
+
+            services.AddDbContext<MedicDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc()
+            .AddSessionStateTempDataProvider();
+
+            services.AddSession();
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<MedicDbContext>()
+               .AddDefaultTokenProviders().AddDefaultUI();
+
             services.AddControllersWithViews();
+            services.AddRazorPages();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,18 +69,24 @@ namespace WebPresentation
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseSession();
+
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
