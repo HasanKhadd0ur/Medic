9b4757007c30d669cf4edad920d55ceac9c9ce0b
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-05-21 22:01:11 +0300

    adding view models

---
+++ ApplicationCore/ViewModel/PateintMedicineViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.ViewModel
+{
+    public class PateintMedicineViewModel
+    {
+        public Patient Patien { get; set; }
+
+        public IEnumerable<Medicine> Medicines { get; set; }
+
+    }
+}
